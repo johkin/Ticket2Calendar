cf1789756daf074fee07f07481dc84f2e6671784
Johan Kindgren <johan.kindgren@acrend.se>
2011-05-01 21:39:29 +0200

    * Ny screenshot * Bättre felmeddelande vid problem med text-tolkning

--- Sj2Calendar/src/se/acrend/sj2cal/parser/MessageParserBase.java
+++ Sj2Calendar/src/se/acrend/sj2cal/parser/MessageParserBase.java
@@ -10,13 +10,13 @@ public class MessageParserBase {
   }
 
   protected String findValue(final String message, final String patternStr, final String fieldName) {
-  
+
     Pattern pattern = Pattern.compile(patternStr);
     Matcher matcher = pattern.matcher(message);
     if (!matcher.find() || matcher.groupCount() != 1) {
-      throw new IllegalArgumentException("Kunde inte tolka " + fieldName);
+      throw new IllegalArgumentException("Kunde inte tolka " + fieldName + ". Meddelande: " + message);
     }
-  
+
     return matcher.group(1);
   }
 
