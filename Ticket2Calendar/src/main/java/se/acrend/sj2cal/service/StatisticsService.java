70a3cbcbcaa7b53b05e63928cbcde4e05e5f8b2b
Johan Kindgren <johan.kindgren@acrend.se>
2013-11-11 08:52:19 +0100

    uppdatering för ny version av Google Analytics

--- Ticket2Calendar/src/main/java/se/acrend/sj2cal/service/StatisticsService.java
+++ Ticket2Calendar/src/main/java/se/acrend/sj2cal/service/StatisticsService.java
@@ -5,7 +5,12 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.Fields;
+import com.google.analytics.tracking.android.MapBuilder;
 import com.google.analytics.tracking.android.Tracker;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StatisticsService extends IntentService {
 
@@ -19,37 +24,19 @@ public class StatisticsService extends IntentService {
 
   @Override
   protected void onHandleIntent(final Intent intent) {
-    Tracker tracker = null;
-    try {
-      tracker = EasyTracker.getTracker();
-
       String category = intent.getStringExtra("category");
       String action = intent.getStringExtra("action");
       String label = intent.getStringExtra("label");
 
       Log.d(TAG, "Send  statistics: " + category + ", " + action + ", " + label);
 
-      tracker.trackEvent(category, action, label, null);
-    } finally {
-      if (tracker != null) {
-        tracker.close();
-      }
-    }
+      easyTracker.send(MapBuilder.createEvent(category, action, label, null).build());
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
-    easyTracker = EasyTracker.getInstance();
-    easyTracker.setContext(this);
-  }
-
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
-    if (easyTracker != null) {
-      easyTracker.dispatch();
-    }
+    easyTracker = EasyTracker.getInstance(this);
   }
 
 }
